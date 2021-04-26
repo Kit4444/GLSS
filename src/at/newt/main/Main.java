8bb7880a61d00e12b97457e6a5a60441e4433e49
Newt <Newt@192.168.0.12>
2021-04-26 22:41:17 +0200

    Initial CP, created Main and plugin.yml

---
+++ src/at/newt/main/Main.java
@@ -0,0 +1,20 @@
+package at.newt.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	/*
+	 * Welcome to the Gameslobby Serversystem
+	 * This system is like the Lobbysystem, just without the Core-System but most same functions
+	 * I wanted to keep it as two different projects, so nothing bad can happen.
+	 */
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
