a0592cac2a4dd65e993ac591ee2d24128cc7461a
Newt <Newt@192.168.0.12>
2021-04-29 12:10:36 +0200

    Added ClassManager in Main/onEnable

--- src/at/newt/main/Main.java
+++ src/at/newt/main/Main.java
@@ -2,6 +2,8 @@ package at.newt.main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import at.newt.mysql.lpb.MySQL;
+
 public class Main extends JavaPlugin{
 	
 	/*
@@ -10,11 +12,17 @@ public class Main extends JavaPlugin{
 	 * I wanted to keep it as two different projects, so nothing bad can happen.
 	 */
 	
+	public static Main instance;
+	public static String mysqlprefix = "§eMySQL §7- ";
+	public static MySQL mysql;
+	
 	public void onEnable() {
-		
+		instance = this;
+		Manager manager = new Manager();
+		manager.init();
 	}
 	
 	public void onDisable() {
-		
+		instance = null;
 	}
 }
